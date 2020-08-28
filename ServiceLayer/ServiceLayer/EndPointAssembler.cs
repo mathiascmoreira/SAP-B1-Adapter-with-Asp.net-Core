f977d6d9b6578b6de470aa6bcdb3063cb6d03d8f
Mathias <mathiascmoreira@gmail.com>
2020-08-27 23:47:45 -0300

    implementing EndPointAssembler and improving project structure

---
+++ ServiceLayer/ServiceLayer/EndPointAssembler.cs
@@ -0,0 +1,134 @@
+﻿using Serialize.Linq.Extensions;
+using Serialize.Linq.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ServiceLayer.ServiceLayer
+{
+    public class EndPointAssembler
+    {
+        public string AssembleEndPoint<T>(List<Expression<Func<T, bool>>> whereClauses)
+        {
+            return GetCommand(whereClauses.First().ToExpressionNode());
+        }
+
+        private string GetCommand(ExpressionNode expressionNode)
+        {
+            switch (expressionNode.NodeType)
+            {
+                case ExpressionType.AndAlso:
+                    return EvaluateBinaryExpression(expressionNode, "and", true);                  
+                case ExpressionType.OrElse:
+                    return EvaluateBinaryExpression(expressionNode, "or");
+                case ExpressionType.GreaterThan:
+                    return EvaluateBinaryExpression(expressionNode, "gt");
+                case ExpressionType.GreaterThanOrEqual:
+                    return EvaluateBinaryExpression(expressionNode, "ge");
+                case ExpressionType.LessThan:
+                    return EvaluateBinaryExpression(expressionNode, "lt");
+                case ExpressionType.LessThanOrEqual:
+                    return EvaluateBinaryExpression(expressionNode, "le");
+                case ExpressionType.Equal:
+                    return EvaluateBinaryExpression(expressionNode, "eq");
+                case ExpressionType.NotEqual:
+                    return EvaluateBinaryExpression(expressionNode, "ne");
+                case ExpressionType.Constant:
+                    return EvaluateConstantExpression(expressionNode);
+                case ExpressionType.MemberAccess:
+                    return EvaluateMemberExpression(expressionNode);                    
+                case ExpressionType.Convert:
+                    return EvaluateConvertExpression(expressionNode);
+                case ExpressionType.Not:
+                    return EvaluateNotExpression(expressionNode);
+                case ExpressionType.Call:
+                    return EvaluateMethodCallExpression(expressionNode);
+                default:
+                    throw new Exception("Invalid Expression");
+            }
+        }
+
+        private string EvaluateBinaryExpression(ExpressionNode expressionNode, string op, bool parentheses = false)
+        {
+            var binaryExpression = (BinaryExpressionNode)expressionNode;
+
+            return parentheses ? 
+                $"({GetCommand(binaryExpression.Left)}) {op} ({GetCommand(binaryExpression.Right)})" : 
+                $"{GetCommand(binaryExpression.Left)} {op} {GetCommand(binaryExpression.Right)}";
+        }
+
+        private string EvaluateConstantExpression(ExpressionNode expressionNode)
+        {
+            var constantExpression = (ConstantExpressionNode)expressionNode;
+
+            switch (constantExpression.Type.Name)
+            {
+                case "System.String":
+                case "System.Char":
+                    return $"'{constantExpression.Value}'";
+                case "System.Boolean":
+                case "System.Byte":
+                case "System.SByte":
+                case "System.Decimal":
+                case "System.Double":
+                case "System.Single":
+                case "System.Float":
+                case "System.Int32":
+                case "System.UInt32":
+                case "System.Int64":
+                case "System.UInt64":
+                case "System.Int16":
+                case "System.UInt16":
+                    return constantExpression.Value.ToString();
+                default:
+                    throw new Exception("Invalid Expression");
+            }
+        }
+
+        private string EvaluateMemberExpression(ExpressionNode expressionNode)
+        {
+            return ((MemberExpressionNode)expressionNode).Member.Signature.Split(' ')[1];
+        }
+
+        private string EvaluateConvertExpression(ExpressionNode expressionNode)
+        {
+            return GetCommand(((UnaryExpressionNode)expressionNode).Operand);
+        }
+
+        private string EvaluateNotExpression(ExpressionNode expressionNode)
+        {
+            return $"not ({GetCommand(((UnaryExpressionNode)expressionNode).Operand)})";
+        }
+
+        private string EvaluateMethodCallExpression(ExpressionNode expressionNode)
+        {
+            var methodCallExpression = (MethodCallExpressionNode)expressionNode;
+
+            if (methodCallExpression.Object.Type.Name != "System.String")
+                throw new Exception("Method Call are only accepted from strings");
+
+            return EvaluateStringMethodCall(methodCallExpression);
+        }
+
+        private string EvaluateStringMethodCall(MethodCallExpressionNode methodCallExpression)
+        {
+            var memberExpression = (MemberExpressionNode)methodCallExpression.Object;
+            var constantExpression = (ConstantExpressionNode)methodCallExpression.Arguments.First();
+
+            switch (methodCallExpression.Method.Signature)
+            {
+                case "Boolean Equals(System.String)":
+                    return $"{EvaluateMemberExpression(memberExpression)} eq '{constantExpression.Value}'";
+                case "Boolean StartsWith(System.String)":
+                    return $"startswith({EvaluateMemberExpression(memberExpression)}, '{constantExpression.Value}')";
+                case "Boolean EndsWith(System.String)":
+                    return $"endswith({EvaluateMemberExpression(memberExpression)}, '{constantExpression.Value}')";
+                case "Boolean Contains(System.String)":
+                    return $"contains({EvaluateMemberExpression(memberExpression)}, '{constantExpression.Value}')";
+                default:
+                    throw new Exception("string only supports Equals, StartsWith, EndsWith and Contains methods");
+            }
+        }
+    }
+}
--- ServiceLayer/ServiceLayer/Entity.cs
+++ ServiceLayer/ServiceLayer/Entity.cs
@@ -11,95 +11,59 @@ namespace ServiceLayer.ServiceLayer
 {
     public class Entity<T>
     {
-        public string Where(Expression<Func<T, bool>> expression)
-        {
-            var expressionNode = expression.Body.ToExpressionNode();
+        private readonly List<Expression<Func<T, bool>>> _whereClauses;
+        private readonly EndPointAssembler _endPointAssembler;
 
-            var result = GetCommand(expressionNode);
-
-            return result;
+        public Entity() {
+            _whereClauses = new List<Expression<Func<T, bool>>>();
+            _endPointAssembler = new EndPointAssembler();
         }
 
-        private string GetCommand(ExpressionNode expressionNode)
+        public Entity<T> Where(Expression<Func<T, bool>> expression)
         {
-            switch (expressionNode.NodeType)
-            {
-                case ExpressionType.AndAlso:
-                    return $"({GetCommand(((BinaryExpressionNode)expressionNode).Left)}) and ({GetCommand(((BinaryExpressionNode)expressionNode).Right)})";
-                case ExpressionType.OrElse:                                  
-                    return $"{GetCommand(((BinaryExpressionNode)expressionNode).Left)} or {GetCommand(((BinaryExpressionNode)expressionNode).Right)}";
-                case ExpressionType.GreaterThan:                             
-                    return $"{GetCommand(((BinaryExpressionNode)expressionNode).Left)} gt {GetCommand(((BinaryExpressionNode)expressionNode).Right)}";
-                case ExpressionType.GreaterThanOrEqual:                      
-                    return $"{GetCommand(((BinaryExpressionNode)expressionNode).Left)} ge {GetCommand(((BinaryExpressionNode)expressionNode).Right)}";
-                case ExpressionType.LessThan:                                
-                    return $"{GetCommand(((BinaryExpressionNode)expressionNode).Left)} lt {GetCommand(((BinaryExpressionNode)expressionNode).Right)}";
-                case ExpressionType.LessThanOrEqual:                         
-                    return $"{GetCommand(((BinaryExpressionNode)expressionNode).Left)} le {GetCommand(((BinaryExpressionNode)expressionNode).Right)}";
-                case ExpressionType.Equal:                                   
-                    return $"{GetCommand(((BinaryExpressionNode)expressionNode).Left)} eq {GetCommand(((BinaryExpressionNode)expressionNode).Right)}";
-                case ExpressionType.NotEqual:
-                    return $"{GetCommand(((BinaryExpressionNode)expressionNode).Left)} ne {GetCommand(((BinaryExpressionNode)expressionNode).Right)}";
-                case ExpressionType.Constant:
-                    return GetConstant((ConstantExpressionNode)expressionNode);
-                case ExpressionType.MemberAccess:
-                    return ((MemberExpressionNode)expressionNode).Member.Signature.Split(' ')[1];
-                case ExpressionType.Convert:
-                    return GetCommand(((UnaryExpressionNode)expressionNode).Operand);
-                case ExpressionType.Not:
-                    return $"not ({GetCommand(((UnaryExpressionNode)expressionNode).Operand)})";
-                case ExpressionType.Call:
-                    return GetCommandFromCall((MethodCallExpressionNode)expressionNode);
-                default:
-                    return null;
-            }
+            _whereClauses.Add(expression);
+
+           // var result = GetCommand(expression.Body.ToExpressionNode());
+
+            return this;
         }
 
-        private string GetCommandFromCall(MethodCallExpressionNode expressionNode)
+        public Entity<T> Select()
         {
-            if (expressionNode.Object.Type.Name == "System.String")
-            {
-                switch (expressionNode.Method.Signature)
-                {
-                    case "Boolean Equals(System.String)":
-                        return $"{((MemberExpressionNode)expressionNode.Object).Member.Signature.Split(' ')[1]} eq '{((ConstantExpressionNode)expressionNode.Arguments.FirstOrDefault()).Value}'";
-                    case "Boolean StartsWith(System.String)":
-                        return $"startswith({((MemberExpressionNode)expressionNode.Object).Member.Signature.Split(' ')[1]}, '{((ConstantExpressionNode)expressionNode.Arguments.FirstOrDefault()).Value}')";
-                    case "Boolean EndsWith(System.String)":
-                        return $"endswith({((MemberExpressionNode)expressionNode.Object).Member.Signature.Split(' ')[1]}, '{((ConstantExpressionNode)expressionNode.Arguments.FirstOrDefault()).Value}')";
-                    case "Boolean Contains(System.String)":
-                        return $"contains({((MemberExpressionNode)expressionNode.Object).Member.Signature.Split(' ')[1]}, '{((ConstantExpressionNode)expressionNode.Arguments.FirstOrDefault()).Value}')";
-                    default:
-                        throw new Exception("Unsupported");
-                }
-            }
-            return string.Empty;
+            return this;
         }
 
-        private string GetConstant(ConstantExpressionNode expressionNode)
+        public Entity<T> Top()
         {
-            switch (expressionNode.Type.Name)
-            {
-                case "System.String":
-                case "System.Char":
-                    return $"'{expressionNode.Value}'";
-                case "System.Boolean":
-                case "System.Byte":
-                case "System.SByte":
-                case "System.Decimal":
-                case "System.Double":
-                case "System.Single":
-                case "System.Float":
-                case "System.Int32":
-                case "System.UInt32":
-                case "System.Int64":
-                case "System.UInt64":
-                case "System.Int16":
-                case "System.UInt16":
-                    return expressionNode.Value.ToString();
-                default:
-                    throw new Exception("Expressao inválida");
-            }
+            return this;
         }
+
+        public Entity<T> OrderBy()
+        {
+            return this;
+        }
+
+        public Entity<T> Skip()
+        {
+            return this;
+        }
+
+        public Entity<T> InlineCount()
+        {
+            return this;
+        }
+
+        public int Count()
+        {
+            return 0;
+        }
+
+        public string GetEndPoint()
+        {
+            return _endPointAssembler.AssembleEndPoint(_whereClauses);
+        }
+
+
+       
     }
 }
